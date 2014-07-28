432fada050518e239aa5281fc974af68360ed0af
S Gorobet <Serghei.Gorobet@cfpb.gov>
2014-07-28 15:23:25 -0400

    add a basic flask app OAH_SETTINGS points to a file with key=value pairs, I use it to turn debugging on

---
+++ app/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config.from_envvar('OAH_SETTINGS')
+
+
+@app.route('/rate-checker')
+def rate_checker():
+    return "Rate checker"
+
+
+@app.route('/county-limit')
+@app.route('/county-limit/list')
+def county_limit():
+    return "County limit"
+
+
+if __name__ == '__main__':
+    app.run()
