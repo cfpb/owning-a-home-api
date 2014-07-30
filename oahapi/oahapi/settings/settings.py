8521e6e03120d8f1c2b649a9cd37cf1f366e9a08
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2014-07-30 20:36:47 +0000

    Making database connection work

--- oahapi/oahapi/settings/settings.py
+++ oahapi/oahapi/settings/settings.py
@@ -57,8 +57,10 @@ WSGI_APPLICATION = 'oahapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': '',
+        'NAME': '', 
+        'USER': '',
+        'PASSWORD': ''
     }
 }
 
