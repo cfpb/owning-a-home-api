b3d1a792b8a2434124fb1a17b336a795cf931507
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2014-07-31 15:24:38 +0000

    Added a fake API as an example of future work

--- oahapi/ratechecker/views.py
+++ oahapi/ratechecker/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 # Create your views here.
+
+@api_view(['GET'])
+def fake_list(request):
+    if request.method == 'GET':
+        return Response({'a':1})
