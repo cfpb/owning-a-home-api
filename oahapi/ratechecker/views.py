ffe7addc39f8cdd2f0458a35d4ee6fcf295e6fe5
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2014-07-31 15:27:37 +0000

    Comments, PEP8

--- oahapi/ratechecker/views.py
+++ oahapi/ratechecker/views.py
@@ -4,9 +4,11 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-# Create your views here.
 
 @api_view(['GET'])
 def fake_list(request):
+    """ This is a just a simple API for example purposes. Let's replace this
+    with a real one as soon as we can. """
+
     if request.method == 'GET':
-        return Response({'a':1})
+        return Response({'a': 1})
