21c8965ce59b1286e3da1a61d6f1431ad7e5c553
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2014-08-18 15:19:10 +0000

    Start adding parameters

--- oahapi/ratechecker/views.py
+++ oahapi/ratechecker/views.py
@@ -3,6 +3,86 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+
+from ratechecker.models import Product
+
+class RateCheckerParameters(object):
+    """ The rate checker API has a long list of 
+    parameters that need to be validated. This class helps with 
+    that. """
+
+    def __init__(self):
+        self.LOAN_TYPES = [c[0] for c in Product.LOAN_TYPE_CHOICES]
+        self.PAYMENT_TYPES = [c[0] for c in Product.PAYMENT_TYPE_CHOICES]
+
+        #Here are parameters that are currently not changeable. 
+        self.lock = '60'
+        self.points = '0'
+        self.property_type = 'SF'
+
+    def set_loan_amount(amount):
+        self.loan_amount = int(amount)
+
+    def set_price(price):
+        self.price = int(price)
+
+    def set_state(state_two_letter):
+        self.state  = state_two_letter
+
+    def set_loan_type(loan_type):
+        if loan_type.upper() in self.LOAN_TYPES:
+            self.loan_type = loan_type.upper()
+        else:
+            raise ValueError('loan_type is not one of acceptable values')
+
+    def set_ficos(minfico, maxfico):
+        minfico = int(minfico)
+        maxfico = int(maxfico)
+
+        if minfico > maxfico:
+            minfico, maxfico = maxfico, minfico
+        
+        # So that results don't overlap. (This behavior is from the 
+        # older version of the API. 
+        if minfico != maxfico:
+            maxfico -= 1
+
+        self.minfico = minfico
+        self.maxfico = maxfico
+
+    def set_rate_structure(rate_structure):
+        rate_structure = rate_structure.upper()
+
+        if rate_structure in self.PAYMENT_TYPE_CHOICES:
+            self.rate_structure = rate_structure
+        else:
+            raise ValueError('rate_structure is not one of acceptable values')
+
+    def set_loan_term(loan_term):
+        self.loan_term = int(loan_term)
+        
+    def set_from_query_params(self, query):
+        try: 
+            loan_amount = query['loan_amount']
+            price = query['price']
+            state = query['state']
+            loan_type = query['loan_type']
+            maxfico = query['maxfico']
+            minfico = query['minfico']
+            rate_structure = query['rate_structure']
+            loan_term = query['loan_term']
+        except KeyError as e:
+            msg = "Required parameter %s is missing" % str(e.args[0])
+            raise KeyError(msg)
+
+        self.set_loan_amount(loan_amount)
+        self.set_price(price)
+        self.set_state(state)
+        self.set_loan_type(loan_type)
+        self.set_ficos(minfico, maxfico)
+        self.set_rate_structure()
+        self.set_loan_term()
 
 
 @api_view(['GET'])
@@ -11,4 +91,13 @@ def fake_list(request):
     with a real one as soon as we can. """
 
     if request.method == 'GET':
+
+        
+        parameters = RateCheckerParameters() 
+        try:
+            parameters.set_from_query_params(request.QUERY_PARAMS)
+        except KeyError as e:
+            error_response = {'detail':str(e.args[0])}
+            return Response(error_response, status=status.HTTP_400_BAD_REQUEST)
+
         return Response({'a': 1})
