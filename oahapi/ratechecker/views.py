a868058474a1a6c2e4c2a8b70422852eaf62b0b5
Shashank Khandelwal <shashank.khandelwal@cfpb.gov>
2014-08-18 19:45:55 +0000

    Better error handling for arm_type

--- oahapi/ratechecker/views.py
+++ oahapi/ratechecker/views.py
@@ -21,22 +21,22 @@ class RateCheckerParameters(object):
         self.points = '0'
         self.property_type = 'SF'
 
-    def set_loan_amount(amount):
+    def set_loan_amount(self, amount):
         self.loan_amount = int(amount)
 
-    def set_price(price):
+    def set_price(self, price):
         self.price = int(price)
 
-    def set_state(state_two_letter):
+    def set_state(self, state_two_letter):
         self.state  = state_two_letter
 
-    def set_loan_type(loan_type):
+    def set_loan_type(self, loan_type):
         if loan_type.upper() in self.LOAN_TYPES:
             self.loan_type = loan_type.upper()
         else:
             raise ValueError('loan_type is not one of acceptable values')
 
-    def set_ficos(minfico, maxfico):
+    def set_ficos(self, minfico, maxfico):
         minfico = int(minfico)
         maxfico = int(maxfico)
 
@@ -51,15 +51,21 @@ class RateCheckerParameters(object):
         self.minfico = minfico
         self.maxfico = maxfico
 
-    def set_rate_structure(rate_structure):
+    def set_rate_structure(self, rate_structure, arm_type):
         rate_structure = rate_structure.upper()
 
-        if rate_structure in self.PAYMENT_TYPE_CHOICES:
+        if rate_structure in self.PAYMENT_TYPES:
             self.rate_structure = rate_structure
         else:
             raise ValueError('rate_structure is not one of acceptable values')
 
-    def set_loan_term(loan_term):
+        if rate_structure == Product.ARM:
+            if arm_type is not None and arm_type in Product.ARM_TYPES:
+                self.arm_type = arm_type
+            else:
+                raise ValueError('You must provide an arm_type.')
+
+    def set_loan_term(self, loan_term):
         self.loan_term = int(loan_term)
         
     def set_from_query_params(self, query):
@@ -70,8 +76,9 @@ class RateCheckerParameters(object):
             loan_type = query['loan_type']
             maxfico = query['maxfico']
             minfico = query['minfico']
-            rate_structure = query['rate_structure']
             loan_term = query['loan_term']
+            rate_structure = query['rate_structure']
+            arm_type = query.get('arm_type', None)
         except KeyError as e:
             msg = "Required parameter %s is missing" % str(e.args[0])
             raise KeyError(msg)
@@ -81,8 +88,8 @@ class RateCheckerParameters(object):
         self.set_state(state)
         self.set_loan_type(loan_type)
         self.set_ficos(minfico, maxfico)
-        self.set_rate_structure()
-        self.set_loan_term()
+        self.set_rate_structure(rate_structure, arm_type)
+        self.set_loan_term(loan_term)
 
 
 @api_view(['GET'])
@@ -91,7 +98,6 @@ def fake_list(request):
     with a real one as soon as we can. """
 
     if request.method == 'GET':
-
         
         parameters = RateCheckerParameters() 
         try:
