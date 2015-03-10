bfe456b6f59f0509882fd313928138661ad21cc0
S Gorobet <Serghei.Gorobet@cfpb.gov>
2015-03-10 15:57:25 -0400

    fields are nullable

--- ratechecker/migrations/0002_auto__chg_field_adjustment_min_ltv__chg_field_adjustment_max_ltv__chg_.py
+++ ratechecker/migrations/0002_auto__chg_field_adjustment_min_ltv__chg_field_adjustment_max_ltv__chg_.py
@@ -10,16 +10,16 @@ class Migration(SchemaMigration):
     def forwards(self, orm):
 
         # Changing field 'Adjustment.min_ltv'
-        db.alter_column(u'ratechecker_adjustment', 'min_ltv', self.gf('django.db.models.fields.DecimalField')(default=None, max_digits=6, decimal_places=3))
+        db.alter_column(u'ratechecker_adjustment', 'min_ltv', self.gf('django.db.models.fields.DecimalField')(null=True, max_digits=6, decimal_places=3))
 
         # Changing field 'Adjustment.max_ltv'
-        db.alter_column(u'ratechecker_adjustment', 'max_ltv', self.gf('django.db.models.fields.DecimalField')(default=None, max_digits=6, decimal_places=3))
+        db.alter_column(u'ratechecker_adjustment', 'max_ltv', self.gf('django.db.models.fields.DecimalField')(null=True, max_digits=6, decimal_places=3))
 
         # Changing field 'Product.max_ltv'
-        db.alter_column(u'ratechecker_product', 'max_ltv', self.gf('django.db.models.fields.DecimalField')(max_digits=6, decimal_places=3))
+        db.alter_column(u'ratechecker_product', 'max_ltv', self.gf('django.db.models.fields.DecimalField')(null=True, max_digits=6, decimal_places=3))
 
         # Changing field 'Product.min_ltv'
-        db.alter_column(u'ratechecker_product', 'min_ltv', self.gf('django.db.models.fields.DecimalField')(max_digits=6, decimal_places=3))
+        db.alter_column(u'ratechecker_product', 'min_ltv', self.gf('django.db.models.fields.DecimalField')(null=True, max_digits=6, decimal_places=3))
 
     def backwards(self, orm):
 
@@ -30,10 +30,10 @@ class Migration(SchemaMigration):
         db.alter_column(u'ratechecker_adjustment', 'max_ltv', self.gf('django.db.models.fields.FloatField')(null=True))
 
         # Changing field 'Product.max_ltv'
-        db.alter_column(u'ratechecker_product', 'max_ltv', self.gf('django.db.models.fields.FloatField')())
+        db.alter_column(u'ratechecker_product', 'max_ltv', self.gf('django.db.models.fields.FloatField')(default=None))
 
         # Changing field 'Product.min_ltv'
-        db.alter_column(u'ratechecker_product', 'min_ltv', self.gf('django.db.models.fields.FloatField')())
+        db.alter_column(u'ratechecker_product', 'min_ltv', self.gf('django.db.models.fields.FloatField')(default=None))
 
     models = {
         u'ratechecker.adjustment': {
@@ -44,10 +44,10 @@ class Migration(SchemaMigration):
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'max_fico': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
             'max_loan_amt': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '12', 'decimal_places': '2'}),
-            'max_ltv': ('django.db.models.fields.DecimalField', [], {'max_digits': '6', 'decimal_places': '3'}),
+            'max_ltv': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '6', 'decimal_places': '3'}),
             'min_fico': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
             'min_loan_amt': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '12', 'decimal_places': '2'}),
-            'min_ltv': ('django.db.models.fields.DecimalField', [], {'max_digits': '6', 'decimal_places': '3'}),
+            'min_ltv': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '6', 'decimal_places': '3'}),
             'product': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['ratechecker.Product']"}),
             'prop_type': ('django.db.models.fields.CharField', [], {'max_length': '10', 'null': 'True'}),
             'rule_id': ('django.db.models.fields.IntegerField', [], {}),
@@ -73,10 +73,10 @@ class Migration(SchemaMigration):
             'loan_type': ('django.db.models.fields.CharField', [], {'max_length': '12'}),
             'max_fico': ('django.db.models.fields.IntegerField', [], {}),
             'max_loan_amt': ('django.db.models.fields.DecimalField', [], {'max_digits': '12', 'decimal_places': '2'}),
-            'max_ltv': ('django.db.models.fields.DecimalField', [], {'max_digits': '6', 'decimal_places': '3'}),
+            'max_ltv': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '6', 'decimal_places': '3'}),
             'min_fico': ('django.db.models.fields.IntegerField', [], {}),
             'min_loan_amt': ('django.db.models.fields.DecimalField', [], {'max_digits': '12', 'decimal_places': '2'}),
-            'min_ltv': ('django.db.models.fields.DecimalField', [], {'max_digits': '6', 'decimal_places': '3'}),
+            'min_ltv': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '6', 'decimal_places': '3'}),
             'plan_id': ('django.db.models.fields.IntegerField', [], {'primary_key': 'True'}),
             'pmt_type': ('django.db.models.fields.CharField', [], {'default': "'FIXED'", 'max_length': '12'}),
             'single_family': ('django.db.models.fields.BooleanField', [], {'default': 'True'})
