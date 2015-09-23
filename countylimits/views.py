b3b794a770b5bb52777ac63b9e8cc45274298393
Amy Mok <amy.mok@cfpb.gov>
2015-09-23 13:18:31 -0400

    Change invalid state response to 400

--- countylimits/views.py
+++ countylimits/views.py
@@ -13,8 +13,13 @@ def county_limits(request):
     if request.method == 'GET':
         package = {'request': {}, 'data': []}
         if 'state' in request.GET:
-            state = package['request']['state'] = request.GET['state']
-            package['data'] = CountyLimit.county_limits_by_state(state)
-            return Response(package)
+            state = request.GET['state']
+            data = CountyLimit.county_limits_by_state(state)
+            if data:
+                package['request']['state'] = request.GET['state']
+                package['data'] = data
+                return Response(package)
+            else:
+                return Response({'state': 'Invalid state'}, status=status.HTTP_400_BAD_REQUEST)
         else:
             return Response({'detail': 'Required parameter state is missing'}, status=status.HTTP_400_BAD_REQUEST)
