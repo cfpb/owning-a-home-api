7fe8849832802dc2af721c711f178fb5ce874cdc
Betsy Lorton <elorton@gmail.com>
2018-07-23 18:32:59 -0400

    Replace south with built-in django migrations
    I ran these commands:
    python manage.py makemigrations countylimits
    python manage.py makemigrations ratechecker
    
    This created migration files to reflect the current state of the models
    in the app.
    
    Deleted the migrations that used South, since they are now redundant.

--- ratechecker/migrations/0001_initial.py
+++ ratechecker/migrations/0001_initial.py
@@ -1,161 +1,113 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import localflavor.us.models
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Product'
-        db.create_table(u'ratechecker_product', (
-            ('plan_id', self.gf('django.db.models.fields.IntegerField')(primary_key=True)),
-            ('institution', self.gf('django.db.models.fields.CharField')(max_length=16)),
-            ('loan_purpose', self.gf('django.db.models.fields.CharField')(max_length=12)),
-            ('pmt_type', self.gf('django.db.models.fields.CharField')(default='FIXED', max_length=12)),
-            ('loan_type', self.gf('django.db.models.fields.CharField')(max_length=12)),
-            ('loan_term', self.gf('django.db.models.fields.IntegerField')()),
-            ('int_adj_term', self.gf('django.db.models.fields.IntegerField')(null=True)),
-            ('adj_period', self.gf('django.db.models.fields.PositiveSmallIntegerField')(null=True)),
-            ('io', self.gf('django.db.models.fields.BooleanField')()),
-            ('arm_index', self.gf('django.db.models.fields.CharField')(max_length=96, null=True)),
-            ('int_adj_cap', self.gf('django.db.models.fields.IntegerField')(null=True)),
-            ('annual_cap', self.gf('django.db.models.fields.IntegerField')(null=True)),
-            ('loan_cap', self.gf('django.db.models.fields.IntegerField')(null=True)),
-            ('arm_margin', self.gf('django.db.models.fields.DecimalField')(null=True, max_digits=6, decimal_places=4)),
-            ('ai_value', self.gf('django.db.models.fields.DecimalField')(null=True, max_digits=6, decimal_places=4)),
-            ('min_ltv', self.gf('django.db.models.fields.FloatField')()),
-            ('max_ltv', self.gf('django.db.models.fields.FloatField')()),
-            ('min_fico', self.gf('django.db.models.fields.IntegerField')()),
-            ('max_fico', self.gf('django.db.models.fields.IntegerField')()),
-            ('min_loan_amt', self.gf('django.db.models.fields.DecimalField')(max_digits=12, decimal_places=2)),
-            ('max_loan_amt', self.gf('django.db.models.fields.DecimalField')(max_digits=12, decimal_places=2)),
-            ('single_family', self.gf('django.db.models.fields.BooleanField')(default=True)),
-            ('condo', self.gf('django.db.models.fields.BooleanField')(default=False)),
-            ('coop', self.gf('django.db.models.fields.BooleanField')(default=False)),
-            ('data_timestamp', self.gf('django.db.models.fields.DateTimeField')()),
-        ))
-        db.send_create_signal(u'ratechecker', ['Product'])
+    dependencies = [
+    ]
 
-        # Adding model 'Adjustment'
-        db.create_table(u'ratechecker_adjustment', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('rule_id', self.gf('django.db.models.fields.IntegerField')()),
-            ('product', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['ratechecker.Product'])),
-            ('affect_rate_type', self.gf('django.db.models.fields.CharField')(max_length=1)),
-            ('adj_value', self.gf('django.db.models.fields.DecimalField')(null=True, max_digits=6, decimal_places=3)),
-            ('min_loan_amt', self.gf('django.db.models.fields.DecimalField')(null=True, max_digits=12, decimal_places=2)),
-            ('max_loan_amt', self.gf('django.db.models.fields.DecimalField')(null=True, max_digits=12, decimal_places=2)),
-            ('prop_type', self.gf('django.db.models.fields.CharField')(max_length=10, null=True)),
-            ('min_fico', self.gf('django.db.models.fields.IntegerField')(null=True)),
-            ('max_fico', self.gf('django.db.models.fields.IntegerField')(null=True)),
-            ('min_ltv', self.gf('django.db.models.fields.FloatField')(null=True)),
-            ('max_ltv', self.gf('django.db.models.fields.FloatField')(null=True)),
-            ('state', self.gf('localflavor.us.models.USStateField')(max_length=2, null=True)),
-            ('data_timestamp', self.gf('django.db.models.fields.DateTimeField')()),
-        ))
-        db.send_create_signal(u'ratechecker', ['Adjustment'])
-
-        # Adding model 'Region'
-        db.create_table(u'ratechecker_region', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('region_id', self.gf('django.db.models.fields.IntegerField')(db_index=True)),
-            ('state_id', self.gf('localflavor.us.models.USStateField')(max_length=2)),
-            ('data_timestamp', self.gf('django.db.models.fields.DateTimeField')()),
-        ))
-        db.send_create_signal(u'ratechecker', ['Region'])
-
-        # Adding model 'Rate'
-        db.create_table(u'ratechecker_rate', (
-            ('rate_id', self.gf('django.db.models.fields.IntegerField')(primary_key=True)),
-            ('product', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['ratechecker.Product'])),
-            ('region_id', self.gf('django.db.models.fields.IntegerField')()),
-            ('lock', self.gf('django.db.models.fields.PositiveSmallIntegerField')()),
-            ('base_rate', self.gf('django.db.models.fields.DecimalField')(max_digits=6, decimal_places=3)),
-            ('total_points', self.gf('django.db.models.fields.DecimalField')(max_digits=6, decimal_places=3)),
-            ('data_timestamp', self.gf('django.db.models.fields.DateTimeField')()),
-        ))
-        db.send_create_signal(u'ratechecker', ['Rate'])
-
-
-    def backwards(self, orm):
-        # Deleting model 'Product'
-        db.delete_table(u'ratechecker_product')
-
-        # Deleting model 'Adjustment'
-        db.delete_table(u'ratechecker_adjustment')
-
-        # Deleting model 'Region'
-        db.delete_table(u'ratechecker_region')
-
-        # Deleting model 'Rate'
-        db.delete_table(u'ratechecker_rate')
-
-
-    models = {
-        u'ratechecker.adjustment': {
-            'Meta': {'object_name': 'Adjustment'},
-            'adj_value': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '6', 'decimal_places': '3'}),
-            'affect_rate_type': ('django.db.models.fields.CharField', [], {'max_length': '1'}),
-            'data_timestamp': ('django.db.models.fields.DateTimeField', [], {}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'max_fico': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
-            'max_loan_amt': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '12', 'decimal_places': '2'}),
-            'max_ltv': ('django.db.models.fields.FloatField', [], {'null': 'True'}),
-            'min_fico': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
-            'min_loan_amt': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '12', 'decimal_places': '2'}),
-            'min_ltv': ('django.db.models.fields.FloatField', [], {'null': 'True'}),
-            'product': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['ratechecker.Product']"}),
-            'prop_type': ('django.db.models.fields.CharField', [], {'max_length': '10', 'null': 'True'}),
-            'rule_id': ('django.db.models.fields.IntegerField', [], {}),
-            'state': ('localflavor.us.models.USStateField', [], {'max_length': '2', 'null': 'True'})
-        },
-        u'ratechecker.product': {
-            'Meta': {'object_name': 'Product'},
-            'adj_period': ('django.db.models.fields.PositiveSmallIntegerField', [], {'null': 'True'}),
-            'ai_value': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '6', 'decimal_places': '4'}),
-            'annual_cap': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
-            'arm_index': ('django.db.models.fields.CharField', [], {'max_length': '96', 'null': 'True'}),
-            'arm_margin': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '6', 'decimal_places': '4'}),
-            'condo': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'coop': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'data_timestamp': ('django.db.models.fields.DateTimeField', [], {}),
-            'institution': ('django.db.models.fields.CharField', [], {'max_length': '16'}),
-            'int_adj_cap': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
-            'int_adj_term': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
-            'io': ('django.db.models.fields.BooleanField', [], {}),
-            'loan_cap': ('django.db.models.fields.IntegerField', [], {'null': 'True'}),
-            'loan_purpose': ('django.db.models.fields.CharField', [], {'max_length': '12'}),
-            'loan_term': ('django.db.models.fields.IntegerField', [], {}),
-            'loan_type': ('django.db.models.fields.CharField', [], {'max_length': '12'}),
-            'max_fico': ('django.db.models.fields.IntegerField', [], {}),
-            'max_loan_amt': ('django.db.models.fields.DecimalField', [], {'max_digits': '12', 'decimal_places': '2'}),
-            'max_ltv': ('django.db.models.fields.FloatField', [], {}),
-            'min_fico': ('django.db.models.fields.IntegerField', [], {}),
-            'min_loan_amt': ('django.db.models.fields.DecimalField', [], {'max_digits': '12', 'decimal_places': '2'}),
-            'min_ltv': ('django.db.models.fields.FloatField', [], {}),
-            'plan_id': ('django.db.models.fields.IntegerField', [], {'primary_key': 'True'}),
-            'pmt_type': ('django.db.models.fields.CharField', [], {'default': "'FIXED'", 'max_length': '12'}),
-            'single_family': ('django.db.models.fields.BooleanField', [], {'default': 'True'})
-        },
-        u'ratechecker.rate': {
-            'Meta': {'object_name': 'Rate'},
-            'base_rate': ('django.db.models.fields.DecimalField', [], {'max_digits': '6', 'decimal_places': '3'}),
-            'data_timestamp': ('django.db.models.fields.DateTimeField', [], {}),
-            'lock': ('django.db.models.fields.PositiveSmallIntegerField', [], {}),
-            'product': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['ratechecker.Product']"}),
-            'rate_id': ('django.db.models.fields.IntegerField', [], {'primary_key': 'True'}),
-            'region_id': ('django.db.models.fields.IntegerField', [], {}),
-            'total_points': ('django.db.models.fields.DecimalField', [], {'max_digits': '6', 'decimal_places': '3'})
-        },
-        u'ratechecker.region': {
-            'Meta': {'object_name': 'Region'},
-            'data_timestamp': ('django.db.models.fields.DateTimeField', [], {}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'region_id': ('django.db.models.fields.IntegerField', [], {'db_index': 'True'}),
-            'state_id': ('localflavor.us.models.USStateField', [], {'max_length': '2'})
-        }
-    }
-
-    complete_apps = ['ratechecker']
+    operations = [
+        migrations.CreateModel(
+            name='Adjustment',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('rule_id', models.IntegerField()),
+                ('affect_rate_type', models.CharField(max_length=1, choices=[(b'P', b'Points'), (b'R', b'Rate')])),
+                ('adj_value', models.DecimalField(null=True, max_digits=6, decimal_places=3)),
+                ('min_loan_amt', models.DecimalField(null=True, max_digits=12, decimal_places=2)),
+                ('max_loan_amt', models.DecimalField(null=True, max_digits=12, decimal_places=2)),
+                ('prop_type', models.CharField(max_length=18, null=True, choices=[(b'CONDO', b'Condo'), (b'COOP', b'Co-op'), (b'CASHOUT-REFI', b'Cash-out refinance')])),
+                ('min_fico', models.IntegerField(null=True)),
+                ('max_fico', models.IntegerField(null=True)),
+                ('min_ltv', models.DecimalField(null=True, max_digits=6, decimal_places=3)),
+                ('max_ltv', models.DecimalField(null=True, max_digits=6, decimal_places=3)),
+                ('state', localflavor.us.models.USStateField(max_length=2, null=True)),
+                ('data_timestamp', models.DateTimeField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Fee',
+            fields=[
+                ('fee_id', models.AutoField(serialize=False, primary_key=True)),
+                ('product_id', models.IntegerField()),
+                ('state_id', localflavor.us.models.USStateField(max_length=2)),
+                ('lender', models.CharField(max_length=16)),
+                ('single_family', models.BooleanField(default=True)),
+                ('condo', models.BooleanField(default=False)),
+                ('coop', models.BooleanField(default=False)),
+                ('origination_dollar', models.DecimalField(max_digits=8, decimal_places=2)),
+                ('origination_percent', models.DecimalField(max_digits=6, decimal_places=3)),
+                ('third_party', models.DecimalField(max_digits=8, decimal_places=2)),
+                ('data_timestamp', models.DateTimeField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Product',
+            fields=[
+                ('plan_id', models.IntegerField(serialize=False, primary_key=True)),
+                ('institution', models.CharField(max_length=16)),
+                ('loan_purpose', models.CharField(max_length=12, choices=[(b'REFI', b'Refinance'), (b'PURCH', b'Purchase')])),
+                ('pmt_type', models.CharField(default=b'FIXED', max_length=12, choices=[(b'FIXED', b'Fixed Rate Mortgage'), (b'ARM', b'Adjustable Rate Mortgage')])),
+                ('loan_type', models.CharField(max_length=12, choices=[(b'JUMBO', b'Jumbo Mortgage'), (b'CONF', b'Conforming Loan'), (b'AGENCY', b'Agency Loan'), (b'FHA', b'Federal Housing Administration Loan'), (b'VA', b'Veterans Affairs Loan'), (b'VA-HB', b'VA-HB Loan'), (b'FHA-HB', b'FHA-HB Loan')])),
+                ('loan_term', models.IntegerField()),
+                ('int_adj_term', models.IntegerField(help_text=b'1st part of the ARM definition. E.g. 5 in 5/1 ARM', null=True)),
+                ('adj_period', models.PositiveSmallIntegerField(null=True)),
+                ('io', models.BooleanField()),
+                ('arm_index', models.CharField(max_length=96, null=True)),
+                ('int_adj_cap', models.IntegerField(help_text=b'Max percentage points the rate can adjust at first adjustment.', null=True)),
+                ('annual_cap', models.IntegerField(help_text=b'Max percentage points adjustable at each subsequent adjustment', null=True)),
+                ('loan_cap', models.IntegerField(help_text=b'Total lifetime maximum change that the ARM rate can have.', null=True)),
+                ('arm_margin', models.DecimalField(null=True, max_digits=6, decimal_places=4)),
+                ('ai_value', models.DecimalField(null=True, max_digits=6, decimal_places=4)),
+                ('min_ltv', models.DecimalField(null=True, max_digits=6, decimal_places=3)),
+                ('max_ltv', models.DecimalField(null=True, max_digits=6, decimal_places=3)),
+                ('min_fico', models.IntegerField()),
+                ('max_fico', models.IntegerField()),
+                ('min_loan_amt', models.DecimalField(max_digits=12, decimal_places=2)),
+                ('max_loan_amt', models.DecimalField(max_digits=12, decimal_places=2)),
+                ('single_family', models.BooleanField(default=True)),
+                ('condo', models.BooleanField(default=False)),
+                ('coop', models.BooleanField(default=False)),
+                ('data_timestamp', models.DateTimeField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Rate',
+            fields=[
+                ('rate_id', models.IntegerField(serialize=False, primary_key=True)),
+                ('region_id', models.IntegerField()),
+                ('lock', models.PositiveSmallIntegerField()),
+                ('base_rate', models.DecimalField(max_digits=6, decimal_places=3)),
+                ('total_points', models.DecimalField(max_digits=6, decimal_places=3)),
+                ('data_timestamp', models.DateTimeField()),
+                ('product', models.ForeignKey(to='ratechecker.Product')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Region',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('region_id', models.IntegerField(db_index=True)),
+                ('state_id', localflavor.us.models.USStateField(max_length=2)),
+                ('data_timestamp', models.DateTimeField()),
+            ],
+        ),
+        migrations.AddField(
+            model_name='fee',
+            name='plan',
+            field=models.ForeignKey(to='ratechecker.Product'),
+        ),
+        migrations.AddField(
+            model_name='adjustment',
+            name='product',
+            field=models.ForeignKey(to='ratechecker.Product'),
+        ),
+        migrations.AlterUniqueTogether(
+            name='fee',
+            unique_together=set([('product_id', 'state_id', 'lender', 'single_family', 'condo', 'coop')]),
+        ),
+    ]
