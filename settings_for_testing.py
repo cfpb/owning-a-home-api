dc929c99d1ba60569c39f1fa3e9c8f58cef8b165
Betsy Lorton <elorton@gmail.com>
2018-07-27 12:02:27 -0400

    TEMPLATES setting is required in django 1.10 and beyond

--- settings_for_testing.py
+++ settings_for_testing.py
@@ -53,3 +53,9 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 STATIC_URL = '/static/'
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+    }
+]
