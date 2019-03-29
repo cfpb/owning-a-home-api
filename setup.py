bfc583bbf538f523c518e8fdb284bff26e35c981
Andy Chosak <andy.chosak@cfpb.gov>
2019-03-29 09:13:36 -0400

    Fix Travis and setup.py

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',,
+        'Programming Language :: Python :: 3.6',
         'Framework :: Django',
         'Development Status :: 5 - Production/Stable',
         'Operating System :: OS Independent',
