1347fed8cafbdc031da656cc5f7b03341854b804
Craig Davies <craig.davies@cfpb.gov>
2019-11-08 11:45:35 -0500

    Remove fix_fee_product_index from migration

--- ratechecker/migrations/0002_remove_fee_loader.py
+++ ratechecker/migrations/0002_remove_fee_loader.py
@@ -4,18 +4,6 @@ from __future__ import unicode_literals
 
 from django.db import migrations, OperationalError, ProgrammingError
 
-def fix_fee_product_index(apps, schema_editor):
-    try:
-        schema_editor.execute(
-            'ALTER TABLE IF EXISTS cfpb.ratechecker_fee '
-            'DROP CONSTRAINT IF EXISTS idx_16977_product_id;'
-            'ALTER TABLE IF EXISTS cfpb.ratechecker_fee '
-            'ADD CONSTRAINT idx_16977_product_id '
-            'UNIQUE (product_id, state_id, lender, single_family, condo, coop);'
-        )
-    except (ProgrammingError, OperationalError):
-        pass
-
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -23,7 +11,6 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        #migrations.RunPython(fix_fee_product_index),
         migrations.AlterUniqueTogether(
             name='fee',
             unique_together=set([]),
