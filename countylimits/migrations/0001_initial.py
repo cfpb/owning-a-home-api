97f3644cd71f03f440bb83921ddc6af76b8a8ff9
Will Barton <william.barton@cfpb.gov>
2019-12-17 08:57:46 -0500

    Fix string types for Python 3
    This change removes the leading `b` from strings in the migrations. Python 3 differentiates between strings and bytes, and this causes `makemigrations` to make new migrations to convert all the `b''` strings in the migrations to regular strings. This change avoids creating those superfluous migrations.

--- countylimits/migrations/0001_initial.py
+++ countylimits/migrations/0001_initial.py
@@ -15,8 +15,8 @@ class Migration(migrations.Migration):
             name='County',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('county_fips', models.CharField(help_text=b"A three-digit FIPS code for the state's county", max_length=3)),
-                ('county_name', models.CharField(help_text=b'The county name', max_length=100)),
+                ('county_fips', models.CharField(help_text="A three-digit FIPS code for the state's county", max_length=3)),
+                ('county_name', models.CharField(help_text='The county name', max_length=100)),
             ],
             options={
                 'ordering': ['county_fips'],
@@ -26,9 +26,9 @@ class Migration(migrations.Migration):
             name='CountyLimit',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('fha_limit', models.DecimalField(help_text=b'Federal Housing Administration loan lending limit for the county', max_digits=12, decimal_places=2)),
-                ('gse_limit', models.DecimalField(help_text=b'Loan limit for mortgages acquired by the Government-Sponsored Enterprises', max_digits=12, decimal_places=2)),
-                ('va_limit', models.DecimalField(help_text=b'The Department of Veterans Affairs loan guaranty program limit', max_digits=12, decimal_places=2)),
+                ('fha_limit', models.DecimalField(help_text='Federal Housing Administration loan lending limit for the county', max_digits=12, decimal_places=2)),
+                ('gse_limit', models.DecimalField(help_text='Loan limit for mortgages acquired by the Government-Sponsored Enterprises', max_digits=12, decimal_places=2)),
+                ('va_limit', models.DecimalField(help_text='The Department of Veterans Affairs loan guaranty program limit', max_digits=12, decimal_places=2)),
                 ('county', models.OneToOneField(to='countylimits.County')),
             ],
         ),
@@ -36,8 +36,8 @@ class Migration(migrations.Migration):
             name='State',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('state_fips', models.CharField(help_text=b'A two-digit FIPS code for the state', max_length=2)),
-                ('state_abbr', localflavor.us.models.USStateField(help_text=b'A two-letter state abbreviation', max_length=2)),
+                ('state_fips', models.CharField(help_text='A two-digit FIPS code for the state', max_length=2)),
+                ('state_abbr', localflavor.us.models.USStateField(help_text='A two-letter state abbreviation', max_length=2)),
             ],
             options={
                 'ordering': ['state_fips'],
