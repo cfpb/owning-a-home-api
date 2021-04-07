f906e5f48f6978d553851251068e35db82a08019
Will Barton <william.barton@cfpb.gov>
2021-04-07 10:31:24 -0400

    Remove CSRF middleware in settings

--- settings_for_testing.py
+++ settings_for_testing.py
@@ -21,7 +21,6 @@ INSTALLED_APPS = (
 MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
